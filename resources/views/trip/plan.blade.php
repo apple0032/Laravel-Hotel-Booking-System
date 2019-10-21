aa9db080c9b4a0a9484b0ff64429ef8a64ba884c
ken.ip <ken.ip@192.168.1.182>
2019-10-21 16:13:31 +0800

    [NEW]Add loading animation in gen page UI

--- resources/views/trip/plan.blade.php
+++ resources/views/trip/plan.blade.php
@@ -421,11 +421,63 @@
         .skipmap_btn, .openmap_btn{
             cursor: pointer;
         }
+        
+        .lds-ring {
+            display: inline-block;
+            position: relative;
+            width: 64px;
+            height: 64px;
+        }
+        .lds-ring div {
+          box-sizing: border-box;
+          display: block;
+          position: absolute;
+          width: 51px;
+          height: 51px;
+          margin: 6px;
+          border: 6px solid #fff;
+          border-radius: 50%;
+          animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+          border-color: #fff transparent transparent transparent;
+        }
+        .lds-ring div:nth-child(1) {
+          animation-delay: -0.45s;
+        }
+        .lds-ring div:nth-child(2) {
+          animation-delay: -0.3s;
+        }
+        .lds-ring div:nth-child(3) {
+          animation-delay: -0.15s;
+        }
+        @keyframes lds-ring {
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+
+        .loading_css{
+            width: 100%;
+            text-align: center;
+            margin-top: 100px;
+            zoom: 150%;
+            display: none;
+        }
     </style>
 
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
 
-
+    <div class="loading_css">
+        <div class="lds-ring">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+    
     <div class="step-1 animated">
         <div class="row plan_box">
             <div class="col-md-12 plan_box_title">
@@ -1007,6 +1059,10 @@
                     point:point
                 },
                 dataType: 'JSON',
+                beforeSend: function () {
+                    $('.step-4').hide();
+                    $('.loading_css').show();
+                },
                 success: function (data) {
                     if(data['status'] === 'success') {
                         location.href = "../trip/itinerary/"+data['id'];
